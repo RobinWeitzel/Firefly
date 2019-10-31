6f665849c0b10972fce196d1f6e4b293d8ad9bbc
Robin Weitzel <robin.weitzel@fim-rc.de>
2019-10-31 14:54:13 +0100

    fixed bug in App.js

--- App.js
+++ App.js
@@ -1,29 +1,5 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-  TouchableOpacity
-} from 'react-native';
-
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import AppNavigator from './navigation/AppNavigator';
 import codePush from "react-native-code-push";
 
 export default class App extends React.Component {
@@ -31,18 +7,3 @@ export default class App extends React.Component {
     return <AppNavigator />;
   }
 }
-
-/*const App: () => React$Node = () => {
-  const onButtonPress = () => {
-    codePush.sync({
-      updateDialog: true,
-      installMode: codePush.InstallMode.IMMEDIATE
-    });
-  }
-
-  return (
-    <AppNavigator />
-  );
-};
-
-export default App;*/
