eae512c0da2bf558275438e7e1fa39e0c4c9e48f
Robin Weitzel <robin.weitzel@fim-rc.de>
2019-11-01 08:53:38 +0100

    fixed bug in settings screen

--- screens/Settings/SettingsScreen.js
+++ screens/Settings/SettingsScreen.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { View, ScrollView, Text, TextInput, Button, ToastAndroid, Keyboard} from 'react-native';
 import Storage from '../../helpers/Storage';
 
-export default class AssetAccountsScreen extends React.Component {
+export default class SettingsScreen extends React.Component {
     static navigationOptions = {
         title: 'Settings'
     };
@@ -27,11 +27,11 @@ export default class AssetAccountsScreen extends React.Component {
         return (
             <View style={{ flex: 1, padding: 20 }}>
                 <Text style={{ fontSize: 20, fontWeight: "bold" }}>Firefly URL</Text>
-                <TextInput placeholder="URL" value={this.state.url} style={{ fontSize: 20 }} onChangeText={url => this.setState({ url })} />
+                <TextInput placeholder="URL" value={this.state.url} style={{ fontSize: 20 }} onChangeText={url => this.setState({ url: url, token: this.state.token })} autoCorrect={false} />
 
                 <Text style={{ fontSize: 20, fontWeight: "bold", marginTop: 20 }}>Firefly API Token</Text>
                 <ScrollView style={{ marginBottom: 20 }}>
-                    <TextInput autoCompleteType="off" autoCorrect={false} placeholder="API Token" value={this.state.token} style={{ fontSize: 20, textAlignVertical: "top" }} onChangeText={token => this.setState({ token })} multiline numberOfLines={6} />
+                    <TextInput autoCompleteType="off" autoCorrect={false} placeholder="API Token" value={this.state.token} style={{ fontSize: 20, textAlignVertical: "top" }} onChangeText={token => this.setState({ url: this.state.url, token: token })} multiline numberOfLines={6} />
                 </ScrollView>
                 <Button title="Save" color='#009688' onPress={this.save}></Button>
             </View >
